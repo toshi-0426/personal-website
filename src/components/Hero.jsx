68fcd4abfa0a7d61edb5d295c6d77939b14343fa
Toshiyasu Takahashi <toshiyasutakahashi@Toshiyasus-MacBook-Air.local>
2025-02-26 19:03:22 -0800

    finished hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,41 @@
 import React from 'react'
 import { Element } from 'react-scroll'
 import bgImage from '../assets/IMG_4457.jpeg'
+import { LinkedIn, Email } from '@mui/icons-material'
 
 
 const Hero = () => {
   return (
     
     <div 
-      className='h-screen w-full bg-cover bg-center relative'
+      className='h-screen w-full bg-cover bg-center relative overflow-hidden'
       style={{
         backgroundImage: `url(${bgImage})`,
       }}
     >
+      <div className="absolute inset-0 bg-black opacity-30"></div>
       <Element name='home'>
-        <div className='container flex mx-auto mt-16 bg-amber-300'>
-          <h1 className='text-7xl font-bold text-center'>Toshiyasu Takahashi</h1>
+        <div className='absolute inset-0 h-full flex flex-col items-center justify-center text-center text-white'>
+          <h1 className='text-4xl md:text-5xl lg:text-7xl font-bold mb-5 [text-shadow:_0px_0px_3px_rgba(0,0,0,0.5)]'>Toshiyasu Takahashi</h1>
+          <h3 className='font-lb font-thin text-lg md:text-xl lg:text-3xl [text-shadow:_0px_0px_4px_rgba(0,0,0,0.05)]'>CS Student    |    Aspiring Software Engineer</h3>
+          <div className='flex flex-row mt-8 gap-8'>
+            <a 
+              href='https://www.linkedin.com/in/toshiyasu-takahashi/'
+              target="_blank" 
+              rel="noopener noreferrer"
+              className='hover:text-gray-300'
+            >
+              <LinkedIn sx={{ fontSize: 50 }}/>
+            </a>
+            
+            <a 
+              href="mailto:toshiyasutakahashi5@gmail.com"
+              className='hover:text-gray-300'
+            >
+              <Email sx={{ fontSize: 50 }} />
+            </a>
+          </div>
         </div>
-
       </Element>
     </div>
   )
