e4d36befa892958e92052b01c6ce59c5eabbc796
Toshiyasu Takahashi <toshiyasutakahashi@Toshiyasus-MacBook-Air.local>
2025-05-11 20:00:43 +0900

    added some photos and blur on them

--- app/data/photos.js
+++ app/data/photos.js
@@ -1,7 +1,45 @@
+import assembly1 from '@images/assembly1.jpeg'
+import assembly2 from '@images/assembly2.jpeg'
+import breakwaterLighthouse from '@images/breakwater-lighthouse.jpeg'
+import campbellRiver1 from '@images/campbell-river1.jpeg'
+import campbellRiver2 from '@images/campbell-river2.jpeg'
+import campbellRiver3 from '@images/campbell-river3.jpeg'
+import campbellRiver4 from '@images/campbell-river4.jpeg'
+import ferry from '@images/ferry.jpeg'
+import innerHarbor1 from '@images/inner-harbor1.jpeg'
+import innerHarbor2 from '@images/inner-harbor2.jpeg'
+import innerHarbor3 from '@images/inner-harbor3.jpeg'
+import onsen from '@images/izumiya.jpeg'
+import meijiShrine from '@images/meiji-shrine.jpeg'
+import mountain from '@images/nokogirimountain.jpeg'
+import ojiya from '@images/ojiya.jpeg'
+import shinkansen from '@images/shinkansen.jpeg'
+import uvic1 from '@images/uvic1.jpeg'
+import uvic2 from '@images/uvic2.jpeg'
+import uvic3 from '@images/uvic3.jpeg'
+import uvic4 from '@images/uvic4.jpeg'
+
+
 export const photos = [
-    { src: "/images/dog1.png", alt: "Picture of my dog1"}, 
-    { src: "/images/dog2.png", alt: "Picture of my dog2"}, 
-    { src: "/images/dog3.png", alt: "Picture of my dog3"},
-    { src: "/images/dog4.png", alt: "Picture of my dog4"},
+    { src: assembly1, alt: "Legislative Assembly Fountain"}, 
+    { src: assembly2, alt: "Legislative Assembly Fountain"}, 
+    { src: breakwaterLighthouse, alt: "Breakwater Lighthouse"},
+    { src: campbellRiver1, alt: "Campbell River"},
+    { src: campbellRiver2, alt: "Campbell River"},
+    { src: campbellRiver3, alt: "Campbell River"},
+    { src: campbellRiver4, alt: "Campbell River"},
+    { src: ferry, alt: "on the ferry from Victoria to Vancouver"},
+    { src: innerHarbor1, alt: "Inner Harbor in Victoria"},
+    { src: innerHarbor2, alt: "Inner Harbor in Victoria"},
+    { src: innerHarbor3, alt: "Inner Harbor in Victoria"},
+    { src: onsen, alt: "izumiya onsen"},
+    { src: meijiShrine, alt: "Meiji Shrine"},
+    { src: mountain, alt: "Nokogiri mountain"},
+    { src: ojiya, alt: "in ojiya city"},
+    { src: shinkansen, alt: "View from Joetsu Shinkansen"},
+    { src: uvic1, alt: "University of Victoria"},
+    { src: uvic2, alt: "University of Victoria"},
+    { src: uvic3, alt: "University of Victoria"},
+    { src: uvic4, alt: "University of Victoria"},
 ]
     
--- app/photos/page.js
+++ app/photos/page.js
@@ -21,37 +21,10 @@ export default function PhotosPage() {
                         src={photo.src} 
                         className="object-contain w-full h-full"
                         sizes="(max-width: 768px) 100vw, 50vw"
-                        alt={photo.alt} priority={true} quality={50}/>
+                        alt={photo.alt} priority={true} quality={50} placeholder="blur"/>
                     </div>
-                ))}
-                
+                ))} 
             </div>
-            {/*
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div className="relative h-60 overflow-y-hidden">
-                    <Image fill src={dog1} className="object-contain w-full h-full" 
-                    sizes="(max-width: 768px) 100vw, 50vw"
-                    alt="Picture of my dog" priority={true} quality={50} placeholder="blur" /> 
-                </div>
-                <div className="relative h-60 overflow-y-hidden">
-                    <Image fill src={dog2} className="object-contain w-full h-full"
-                    sizes="(max-width: 768px) 100vw, 50vw"
-                    alt="Picture of my dog" priority={true} quality={50} placeholder="blur" /> 
-                </div>
-                <div className="relative h-60 overflow-y-hidden">
-                    <Image fill src={dog3} className="object-contain w-full h-full"
-                    sizes="(max-width: 768px) 100vw, 50vw"
-                    alt="Picture of my dog" quality={50} placeholder="blur" /> 
-                </div>
-                <div className="relative h-60 overflow-y-hidden">
-                    <Image fill src={dog4} className="object-contain w-full h-full"
-                    sizes="(max-width: 768px) 100vw, 50vw"
-                    alt="Picture of my dog" quality={50} placeholder="blur" /> 
-                </div>
-                
-                         
-            </div>
-            */}
         </div>
     )
 }
