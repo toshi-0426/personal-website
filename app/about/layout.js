5c80d923652e7a1fc93031eceb0aae9f5640f5e2
Toshiyasu Takahashi <toshiyasutakahashi@Toshiyasus-MacBook-Air.local>
2025-06-09 17:34:36 +0900

    modified contact placement

--- app/about/layout.js
+++ app/about/layout.js
@@ -3,14 +3,7 @@ export default function AboutLayout({ children }) {
     <div>
       <div>{children}</div>
 
-      <div className="mt-8">
-        <h2 className="mb-4 text-xl">You might also like </h2>
-
-        <ul>
-          <li>First Blog Post</li>
-          <li>Second Blog Post</li>
-        </ul>
-      </div>
+      
     </div>
   );
 }
