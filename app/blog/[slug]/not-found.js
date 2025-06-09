84bfa9663deefe408b68785a13ac709c1cd7dcb1
Toshiyasu Takahashi <toshiyasutakahashi@Toshiyasus-MacBook-Air.local>
2025-06-09 17:02:37 +0900

    modified notfound

--- app/blog/[slug]/not-found.js
+++ app/blog/[slug]/not-found.js
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className='space-y-4'>
       <h1 className="text-2xl">404</h1>
-      <div>The page you're looking for was not found.</div>
+      <div>The page you&apos;re looking for was not found.</div>
       <Link href="/" className="border p-1 rounded-md font-semibold hover:bg-gray-200 dark:hover:bg-gray-800">
           Return Home
       </Link>
